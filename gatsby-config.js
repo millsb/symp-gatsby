176763738eda4539e213d2c55f0c932348368b59
bryan <bryan@millz.net>
2018-09-22 21:16:05 -0400

    Add manual graphql schema fetch to avoid gatsby caching schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+const {introspectionQuery, buildClientSchema} = require("graphql");
+const {request} = require("graphql-request");
+const SC_GRAPHQL_URL = 'https://sitecore-622750-single.azurewebsites.net/sitecore/api/graph/items/master';
 const SC_API_KEY = '75975ED5-7D7A-4E2D-995A-BB526D511A01';
 
 module.exports = {
@@ -34,9 +37,14 @@ module.exports = {
         // This is field under which it's accessible
         fieldName: "sc",
         // Url to query from
-        url: "https://sitecore-622750-single.azurewebsites.net/sitecore/api/graph/items/master",
+        url: SC_GRAPHQL_URL,
         headers: {
           "sc_apikey": SC_API_KEY
+        },
+        createSchema: async () => {
+          const introspectionUrl =  `${SC_GRAPHQL_URL}?sc_apikey=${SC_API_KEY}`;
+          const introspectionData = await request(introspectionUrl, introspectionQuery);
+          return buildClientSchema(introspectionData);
         },
         refetchInterval: 30
       }
