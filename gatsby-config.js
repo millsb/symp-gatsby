8d85862e461556272c613906442f07c48d71ed0a
Bryan Mills <bryan.mills@velir.com>
2018-10-09 18:11:27 -0400

    remove __dirname from filesystem config

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: "images",
-        path: `${__dirname}/src/images/`
+        path: `src/images/`
       }
     },
     {
